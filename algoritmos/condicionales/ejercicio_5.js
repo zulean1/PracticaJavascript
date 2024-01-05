69436dec82e9216e51cd7dc220a8da1bde4998c4
zulean1 <zuleidyrodriguez1@gmail.com>
2024-01-05 16:33:15 -0400

    Ejercicio 5 culminado

--- algoritmos/condicionales/ejercicio_5.js
+++ algoritmos/condicionales/ejercicio_5.js
@@ -1,5 +1,4 @@
-/*Se tiene el nombre y la edad de tres personas.
-Se desea saber el nombre y la edad de la persona de menor edad. 
+/*Se tiene el nombre y la edad de tres personas. Se desea saber el nombre y la edad de la persona de menor edad. 
 Realice el algoritmo correspondiente y represéntelo 
 con un diagrama de flujo, pseudocódigo y diagrama N/S.*/
 
@@ -14,9 +13,18 @@ let name3 = prompt('Ingresa tu nombre');
 let age3 = prompt('Ingrese su edad');
 
 
-if (condition) {
+
+if (age1 < age2 && age1 < age3) {
+    console.log(`El menor es ${name1} porque tiene ${age1}`);
     
-} else {
+} 
+
+else if (age2 < age1 && age2 < age3) {
+    console.log(`El menor es ${name2} porque tiene ${age2}`);
     
+} 
+
+else {
+    console.log(`El menor es ${name3} porque tiene ${age3}`);
 }
 
